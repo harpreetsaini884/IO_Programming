04520d69ecf8181d35750c3a77215db16f4119c5
Harpreet Saini <harpreetsinghsaini884@gmail.com>
2025-04-07 09:26:12 +0530

    [Harpreet] ADD: JSONObject commited

---
+++ Submission of JSON Data/Practice Problems/JSONObject.java
@@ -0,0 +1,30 @@
+
+{
+    "name": "Alice",
+    "age": 21,
+    "subjects": ["Math", "Physics", "Computer Science"]
+  }
+  
+
+// import java.util.*;
+// public class JSONObject{
+//     public static void main(String[] args) {
+//         JSONArray subjects=new JSONArray();
+//         subjects.put("C++");
+//         subjects.put("Java");
+//         subjects.put("Python");
+//         subjects.put("React");
+
+//         JSONObject student=new JSONObject();
+//         student.put("name","Harry");
+//         student.put("Age",23);        
+//         student.put("subjects",subjects);        
+
+//         System.out.println(student.toString());
+//     }
+
+//     private void put(String string, String string2) {
+//         // TODO Auto-generated method stub
+//         throw new UnsupportedOperationException("Unimplemented method 'put'");
+//     }
+// }
